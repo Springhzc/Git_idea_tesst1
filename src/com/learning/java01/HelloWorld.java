30cee6e991da378062e36e19d9175c47f4795e36
Spring <1289863389@qq.com>
2022-04-24 10:56:21 +0800

    test

---
+++ src/com/learning/java01/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.learning.java01;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hellow world");
+        ArrayList list=new ArrayList();
+    }
+    public void method(){
+        System.out.println("hello");
+    }
+
+}
+
---
+++ src/com/learning/java01/test.java
@@ -0,0 +1,11 @@
+package com.learning.java01;
+
+/**
+ * @author Spring
+ * @date 2022/4/22 - 11:23
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
